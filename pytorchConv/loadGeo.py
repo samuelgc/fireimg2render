d3ddaac13f6465cb61aa7b97a093d7eb5faee3e9
Brian Rasmussen <brian.rasmussen55@gmail.com>
2018-06-20 14:29:13 -0600

    parser works

--- pytorchConv/loadGeo.py
+++ pytorchConv/loadGeo.py
@@ -1,6 +1,7 @@
 import simplejson
 import numpy as np
 from PIL import Image
+import collections
 dimX = 35
 dimY = 65
 dimZ = 35
@@ -17,7 +18,6 @@ def getInfo(data):
     ind1 , ind2 = secLine.find("[") , secLine.find("]")
     arrDim = secLine[ind1+1: ind2].split(",")
     dimX , dimY , dimZ = arrDim[:]
-    print arrDim , dimX , dimY , dimZ
     for i , line in enumerate(working):
         if(i == 0):
             continue
@@ -25,21 +25,20 @@ def getInfo(data):
         volumeSum[i - 1] = line[ind1 + 1:ind2]
     print volumeSum
     return volumeSum , int(dimX) , int(dimY) , int(dimZ)
+def printBy16(tile):
+    for st in range(len(tile)//16 - 1):
+        print st , tile[st*16:(st+1)*16]
 def getTileData(tile,mat,xOff,yOff,zOff,comp):
-    print dimX , dimY , dimZ
-    print comp
+    # print comp
+    # Calculating start x y z values for the tile
     x_start = xOff * 16
     y_start = yOff * 16
     z_start = zOff * 16
-    #lengths = 16
-    xLen = 16
-    yLen = 16
-    zLen = 16
-    #if not full tile make lengths real lengths
-    if(comp == 0):
-        xLen = dimX - xOff * 16
-        yLen = dimY - yOff * 16
-        zLen = dimZ - zOff * 16
+    # Calculate the lengths if on edge AKA where lengths < 16
+    xLen = dimX - xOff * 16
+    yLen = dimY - yOff * 16
+    zLen = dimZ - zOff * 16
+    # if not on edge reset to 16
     if xLen > 16:
         xLen = 16
     if yLen > 16:
@@ -48,50 +47,39 @@ def getTileData(tile,mat,xOff,yOff,zOff,comp):
         zLen = 16
     #make np array of data
     arr = np.array(tile)
-    #make default ends
+    arr2 = np.array(tile)
+    #make ends based on previus calculated length and starts
     x_end = x_start + xLen
     y_end = y_start + yLen
     z_end = z_start + zLen
     #if values are all the same
     if(comp == 2):
-        print "entered 2"
-        if x_end > dimX:
-            print "entered X"
-            x_end = dimX
-            xLen = x_end - x_start 
-        if y_end > dimY:
-            print "entered Y"
-            y_end = dimY
-            yLen = y_end - y_start 
-        if z_end > dimZ:
-            print "entered Z"
-            z_end = dimZ
-            zLen = z_end - z_start 
-        arr = np.full((xLen,yLen,zLen),0)        
-    print "xs[{}] ys[{}] zs[{}]\nxe[{}] ye[{}] ze[{}]\nxl[{}] yl[{}] zl[{}]".format(x_start,y_start,z_start,x_end,y_end,z_end,xLen,yLen,zLen)
-
+        arr = np.full((xLen,yLen,zLen),tile) # Both of these methods work 
+        arr2 = np.full(xLen*yLen*zLen,tile) # Both of these methods work 
+    #method one
     arrMat = arr.reshape((xLen,yLen,zLen))
-    # print mat[x_start:x_end,y_start:y_end,z_start:z_end].shape , arrMat.shape
-    mat[x_start:x_end,y_start:y_end,z_start:z_end] = arrMat
+    #method two
+    arrLol = np.empty((xLen,yLen,zLen))
+    count = 0
+    for x in range(xLen):
+        for y in range(yLen):
+            for z in range(zLen):
+                arrLol[x,y,z] = arr2[count]
+                count += 1
+    
+    mat[x_start:x_end,y_start:y_end,z_start:z_end] = arrLol 
+    # mat[x_start:x_end,y_start:y_end,z_start:z_end] = arrMat
 def getVoxelDataAt(data,x,y,z):
     comprArr = data[2][1][3]
     tiles = data[2][1][5]
     mat = np.empty((x,y,z))
-    # print len(tiles)
-    cx , cy , cz = 0,0,0
-    print "there are [{}] tiles".format(len(tiles))
-    for i,tile in enumerate(tiles):
-        print "tile number[{}]".format(i)
-        getTileData(tile[3],mat,cx,cy,cz,tile[1])
-        cx += 1
-        if(cx > dimX // 16):
-            cy += 1
-            cx = 0
-        if(cy > dimY // 16):
-            cz += 1
-            cy = 0
-        if(cz > dimZ // 16):
-            cz = 0
+    tileCount = 0
+    for tx in range((dimX + 15) // 16):
+        for ty in range((dimY + 15) // 16):
+            for tz in range((dimZ + 15) // 16):
+                tile = tiles[tileCount]
+                getTileData(tile[3],mat,tx,ty,tz,tile[1])
+                tileCount +=1
     return mat
 
 
@@ -103,21 +91,11 @@ def getAllVoxelData(geoData,geoInfo):
     return voxelData
 
 if __name__== '__main__':
-    geoData = loadJson("./help.json")
+    geoData = loadJson("./temp.json")
     geoInfo, dimX , dimY , dimZ = getInfo(geoData)
-    # for key in geoInfo.keys():
-    #     print "key[{}]".format(geoInfo[key])
-    # print dimX , dimY , dimZ
     voxelData = getAllVoxelData(geoData,geoInfo)
-    for key in voxelData.keys():
-        print "key[{}] unique[{}]".format(key,len(np.unique(voxelData[key])))
-    density = voxelData["fuel"]
-    slicedDen = density[:,:,17] * 255
-    print np.unique(slicedDen)
-    print slicedDen.shape
-    img = Image.fromarray(slicedDen)
-    img.show()
-
-
-    
-    # print geoInfo
+    density = voxelData["density"]
+    slicedDen = density[:,0,:]
+    for sl in range(len(density)):
+        imgTemp = Image.fromarray(np.uint8(density[:,sl,:] * 255),"L")
+        imgTemp.save("slice[{}].png".format(str(sl + 1).zfill(2)))
