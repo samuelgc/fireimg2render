a82750349d40aaf036a04748cc6a41a5e835bbec
SamuelG-C <samuel.jingc@gmail.com>
2018-08-23 08:58:14 -0600

    debugging model

--- param_train.py
+++ param_train.py
@@ -5,13 +5,11 @@ import tensorflow as tf
 import numpy as np
 import random
 import csv
-import os
 
 
 class ParamLearner:
     input_size = [None, 256, 256, 3]
     output_size = [None, 10]
-    model_directory = '.\models\param_learner_model'
 
     def __init__(self):
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
@@ -28,58 +26,51 @@ class ParamLearner:
         pool2 = tf.layers.max_pooling2d(conv2, 2, 2, padding="same", name='pool2')
 
         #Dense layers
-        flat = tf.reshape(pool2, [-1])
+        flat = tf.reshape(pool2, [1, 16384])
         dense0 = tf.layers.dense(flat, units=1024, activation=tf.nn.relu, name='dense0')
         dense1 = tf.layers.dense(dense0, units=512, activation=tf.nn.relu, name='dense1')
-        self.output = tf.layers.dense(dense1, units=10, activation=tf.nn.sigmoid, name='output')
+        dense2 = tf.layers.dense(dense1, units=256, activation=tf.nn.relu, name='dense2')
+        self.output = tf.layers.dense(dense2, units=10, activation=tf.nn.sigmoid, name='output')
 
         self.loss = tf.reduce_sum(tf.abs(self.target - self.output), name='loss')
-        self.cost = tf.reduce_mean(self.loss)
-        self.train = tf.train.AdamOptimizer().minimize(self.cost, name='train')
+        self.train = tf.train.AdamOptimizer().minimize(self.loss, name='train')
 
-        tf.add_to_collection('input', self.input)
-        tf.add_to_collection('target', self.target)
-        tf.add_to_collection('output', self.output)
-        tf.add_to_collection('loss', self.loss)
-        tf.add_to_collection('cost', self.cost)
-        tf.add_to_collection('train', self.train)
-        self.saver = tf.train.Saver()
-
-    def train(self, input_data, load_existing_model=True):
+    def start_train(self, fresh=True, sample_size=500):
+        if fresh:
+            create_training_file(sample_size)
+            print "New training file generated"
+        data = np.loadtxt('./train_data/shader_params.csv', delimiter=",")
         self.sess.run(tf.global_variables_initializer())
-        if load_existing_model and os.path.exists(self.model_directory):
-            self.load_trained_model()
-        else:
-            self.sess.run(tf.global_variables_initializer())
-
-        for epoch in range(100):
-            count = 0
-            try:
-                params = input_data[count]
-                if epoch == 0:
-                    with open('./ifds/fire.ifd') as f:
-                        search_string = "fc_colorramp_the_basis_strings ( \"linear\" \"linear\" ) fc_colorramp_the_key_positions ( 0 1 ) fc_colorramp_the_key_values ( 0 0 0 1 1 1 )"
-                        replace_string = "s_densityscale {} s_int {} s_color {} {} {} fi_int {} fc_int {} fc_colorramp_the_basis_strings ( \"linear\" \"linear\" ) fc_colorramp_the_key_positions ( 0 1 ) fc_colorramp_the_key_values ( 0 0 0 1 1 1 ) fc_bbtemp {} fc_bbadapt {} fc_bbburn {}" \
-                            .format(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8], params[9])
-                        contents = f.read().replace(search_string, replace_string)
-                    with open('./ifds/render_fire_{}_{}.ifd'.format(epoch, count), "w+") as f:
-                        f.write(contents)
-                    call(["mantra", "./ifds/render_fire_{}_{}.ifd".format(epoch, count), "./render/render_{}_{}.jpg".format(count)])
-                img = Image.open("./render/render_{}_{}.jpg".format(count))
-                img_in = np.asarray(img)
-                feed_dict = {self.input: img_in, self.target: params}
-                loss, _ = self.sess.run([self.cost, self.output], feed_dict=feed_dict)
-                count += 1
-            except Exception as fail:
-                continue
-            finally:
-                self.save_trained_model()
-
-    def save_trained_model(self):
-        self.saver.save(self.sess, os.path.join(self.model_directory, 'model'))
-
-    def load_trained_model(self):
-        self.saver.restore(self.sess, tf.train.latest_checkpoint(self.model_directory))
+        for epoch in range(1000):
+            total_loss = 0
+            for count in range(len(data)):
+                try:
+                    params = data[count]
+                    if fresh and epoch == 0:
+                        with open('./ifds/fire.ifd') as f:
+                            search_string = "fc_colorramp_the_basis_strings ( \"linear\" \"linear\" ) fc_colorramp_the_key_positions ( 0 1 ) fc_colorramp_the_key_values ( 0 0 0 1 1 1 )"
+                            replace_string = "s_densityscale {} s_int {} s_color {} {} {} fi_int {} fc_int {} fc_colorramp_the_basis_strings ( \"linear\" \"linear\" ) fc_colorramp_the_key_positions ( 0 1 ) fc_colorramp_the_key_values ( 0 0 0 1 1 1 ) fc_bbtemp {} fc_bbadapt {} fc_bbburn {}" \
+                                .format(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8], params[9])
+                            contents = f.read().replace(search_string, replace_string)
+                        with open('./ifds/render_fire_{}_{}.ifd'.format(epoch, count), "w+") as f:
+                            f.write(contents)
+                        call(["mantra", "./ifds/render_fire_{}_{}.ifd".format(epoch, count), "./render/render_{}.jpg".format(count)])
+                    img = Image.open("./render/render_{}.jpg".format(count))
+                    img_in = np.asarray(img)
+                    batch_in = []
+                    batch_in.append(img_in)
+                    batch_out = []
+                    batch_out.append(params)
+                    feed_dict = {self.input: batch_in, self.target: batch_out}
+                    loss, output = self.sess.run([self.loss, self.output], feed_dict=feed_dict)
+                    total_loss += loss
+                    count += 1
+                except Exception as fail:
+                    print fail
+                    continue
+            if fresh and epoch == 0:
+                print "Image dataset rendered"
+            print "Epoch: {} --> Average Loss: {}".format(epoch, total_loss / len(data))
 
 
 def generate_samples(size):
@@ -87,17 +78,17 @@ def generate_samples(size):
     for count in range(size):
         sample = []
         #Generate shader parameters randomly
-        sample.append(random.uniform(0, 2))             # Density Scale
-        sample.append(random.uniform(0, 2))             # Smoke Brightness
-        smoke_color = random.random()                   # Smoke Color
+        sample.append(random.uniform(0.5, 1.5))             # Density Scale (0 - 2) default: 1
+        sample.append(random.uniform(0.5, 1.5))             # Smoke Brightness (0 - 2) default: 1
+        smoke_color = random.random()                   # Smoke Color (0 - 1) default: 0.2
         sample.append(smoke_color)
         sample.append(smoke_color)
         sample.append(smoke_color)
-        sample.append(random.uniform(0, 5))             # Intensity Scale
-        sample.append(random.uniform(0, 5))             # Temperature Scale
-        sample.append(int(random.uniform(0, 15000)))    # Color Temp in Kelvin
-        sample.append(random.random())                  # Adaption
-        sample.append(random.uniform(-2, 2))            # Burn
+        sample.append(random.uniform(0.5, 1.5))             # Intensity Scale (0 - 5) default: 2
+        sample.append(random.uniform(0, .4))           # Temperature Scale (0 - 5) default: 0.2
+        sample.append(int(random.uniform(2500, 7500)))  # Color Temp in Kelvin (0 - 15000) default: 5000
+        sample.append(random.uniform(0.05, 0.25))       # Adaption (0 - 1) default: 0.15
+        sample.append(random.uniform(-0.5, 0.5))        # Burn (-2 - 2) default: 0
         samples.append(sample)
     return samples
 
@@ -111,9 +102,7 @@ def create_training_file(size=100):
 
 def main():
     param_learner = ParamLearner()
-    generate_samples(100)
-    data = np.loadtxt('./train_data/shader_params.csv', delimiter=",")
-    param_learner.train(input_data=data)
+    param_learner.start_train(fresh=False)
 
 
 if __name__ == '__main__':
